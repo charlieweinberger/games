f4f0e3e9906a6e579f71d4ee0c318cd745e10f44
Charlie Weinberger <364charliebrown@gmail.com>
2022-03-10 23:43:29 +0000

    got regular game to work

--- game.py
+++ game.py
@@ -1,24 +1,17 @@
 import math
-import itertools
 
 class TicTacToe():
 
     def __init__(self, players, who_goes_first, do_draw_game=False):
         
         self.players = players
+        self.player_order = [who_goes_first, 1 if who_goes_first == 2 else 2]
         self.set_player_numbers()
 
-        self.who_goes_first = who_goes_first
-        self.who_goes_second = 1 if self.who_goes_first == 2 else 2
         self.do_draw_game = do_draw_game
-
-        self.player_moves = {
-            self.who_goes_first: [],
-            self.who_goes_second: [],
-        }
-
+        
         self.game_state = [0 for _ in range(9)]
-
+        
         self.winner = None
 
     def set_player_numbers(self):
@@ -26,88 +19,53 @@ class TicTacToe():
             player.set_player_number(i + 1)
 
     def run_to_completion(self):
-        while self.winner == None:
-            self.complete_round()
+       
+        while self.winner == None: 
+            
+            for player_number in self.player_order:
 
-    def complete_round(self):
+                if self.winner == None:
 
-        for player_number in self.player_moves.keys():
-            if self.winner == None:
+                    self.move(player_number)
+                    self.winner = self.check_for_winner()
 
-                self.move(player_number)
-                self.check_for_winner(player_number)
-                
-                if self.do_draw_game:
-                    self.draw_game()
+                    if self.do_draw_game:
+                        self.draw_game()
 
     def move(self, player_number):
-        both_player_moves = self.player_moves[1] + self.player_moves[2]
-        available_moves = [move for move in range(9) if move not in both_player_moves]
+        available_moves = [elem for elem in range(9) if self.game_state[elem] == 0]
         move = self.players[player_number - 1].move(available_moves)
-        self.player_moves[player_number].append(move)
         self.game_state[move] = player_number
 
-    def check_for_winner(self, player_number):
+    def check_for_winner(self):
 
-        player_moves = self.player_moves[player_number]
-        # print(f'\nPlayer {player_number}: {player_moves}')
-        # print([list(elem) for elem in itertools.combinations(player_moves, 3)])
+        winning_options = [
+            [self.game_state[0], self.game_state[1], self.game_state[2]],
+            [self.game_state[3], self.game_state[4], self.game_state[5]],
+            [self.game_state[6], self.game_state[7], self.game_state[8]],
+            [self.game_state[0], self.game_state[3], self.game_state[6]],
+            [self.game_state[1], self.game_state[4], self.game_state[7]],
+            [self.game_state[2], self.game_state[5], self.game_state[8]],
+            [self.game_state[0], self.game_state[4], self.game_state[8]],
+            [self.game_state[2], self.game_state[4], self.game_state[6]],
+        ]
 
-        for combination in itertools.combinations(player_moves, 3):
-
-            possible_combinations = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-            ]
-
-            if list(combination) in possible_combinations:
-                self.winner = player_number
-                return
+        for arr in winning_options:
+            if arr[0] == arr[1] and arr[1] == arr[2] and arr[0] == arr[2] and arr[0] != 0:
+                return arr[0]
 
         if 0 not in self.game_state:
-            return 'tie'
+            return 0
 
     def draw_game(self):
         
-        rows = [['-' for _ in range(3)] for _ in range(3)]
+        rows = [0 for _ in range(9)]
+
+        for key in self.player_order:
+            for index, x in enumerate(self.game_state):
+                if x == key:
+                    rows[index] = key
         
-        for x in self.player_moves[1]:
-            rows[math.floor(x / 3)][x % 3] = 'X'
-
-        for x in self.player_moves[2]:
-            rows[math.floor(x / 3)][x % 3] = 'O'
-
-        str_row_list = ['', '', '']
-        for i in range(3):
-            for elem in rows[i]:
-                str_row_list[i] += f'{elem} '
-        
-        print('')
-        for elem in str_row_list:
-            print(elem)
-
-    # def combinations(self, player_moves):
-        
-    #     combinations_list = []
-                
-    #     for move_1 in player_moves:
-    #         for move_2 in [move for move in player_moves if move != move_1]:
-    #             for move_3 in [move for move in player_moves if move != move_1 and move != move_2]:
-                    
-    #                 possible_combination = [move_1, move_2, move_3]
-    #                 combination_is_original = True
-                            
-    #                 for combination in combinations_list:
-    #                     if sorted(combination) == sorted(possible_combination):
-    #                         combination_is_original = False
-
-    #                 if combination_is_original:
-    #                     combinations_list.append(possible_combination)
-                
-    #     return combinations_list
+        print(f'\n{rows[0]} {rows[1]} {rows[2]}')
+        print(f'{rows[3]} {rows[4]} {rows[5]}')
+        print(f'{rows[6]} {rows[7]} {rows[8]}')
--- test_game.py
+++ test_game.py
@@ -1,14 +1,15 @@
 import sys
 from game import *
 from random_player import *
+import time
 
 players = [RandomPlayer(), RandomPlayer()]
 
-num_wins = {1: 0, 2: 0, 'tie': 0}
+num_wins = {0: 0, 1: 0, 2: 0}
 num_trials = 1000
 
 for i in range(num_trials):
-        
+    
     game = TicTacToe(players, who_goes_first = (i % 2) + 1, do_draw_game = (i == 0))
     game.run_to_completion()
     num_wins[game.winner] += (100 / num_trials)
@@ -16,5 +17,5 @@ for i in range(num_trials):
     if i == 0:
         print(f"\n{game.winner = }")
 
-num_wins = {key:round(value, 2) for key, value in num_wins.items()}
+num_wins = {key:round(value, 5) for key, value in num_wins.items()}
 print(f"\n{num_wins = }\n")
