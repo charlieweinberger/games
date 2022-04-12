127e718dac0b1be442556f02694382062891d962
Charlie Weinberger <364charliebrown@gmail.com>
2022-04-12 22:46:20 +0000

    checkpoint

--- test_game.py
+++ test_game.py
@@ -43,16 +43,69 @@ def mate(info, i):
     
     return next_gen
 
+def gen_i_vs_j(info, i, j):
+
+    scores = []
+
+    for player_1 in info[i]:
+
+        score = 0
+
+        for player_2 in info[j]:
+
+            if player_1 == player_2: continue
+
+            this_round_players = [player_1, player_2]
+
+            for n in [1, 2]:
+
+                game = TicTacToe(this_round_players, who_goes_first=n)
+                game.run_to_completion()
+
+                if   game.winner == '1': score -= 1
+                elif game.winner == '2': score += 1
+
+        scores.append(score)
+
+    return scores
+
+# code
+
 info = {0: {Player() : 0 for _ in range(25)}}
 
-number_of_generations = 15
+number_of_generations = 25
+
+plots = {
+    # 'gen 0 vs gen i': []
+    'gen i-1 vs gen i': []
+}
 
 for i in range(number_of_generations):
-    print(f'\n{i = }')
+    
     have_strategies_fight(info, i)
+
+    # plot_1 = gen_i_vs_j(info, 0, i)
+    # plots['gen 0 vs gen i'].append(sum(plot_1) / len(plot_1))
+
+    if i != 0:
+        plot_2 = gen_i_vs_j(info, i-1, i)
+        plots['gen i-1 vs gen i'].append(sum(plot_2) / len(plot_2))
+    
     info[i + 1] = mate(info, i)
 
-    print(list(info[i].values()))
-    print(sorted(list(info[i].values())))
+# plotting
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
+
+# plt.figure(1)
+# plt.plot(list(range(number_of_generations)), plots['gen 0 vs gen i'])
+# plt.xlabel('generation number')
+# plt.ylabel('score vs 1st generation')
+# plt.savefig('plot_1.png')
+
+# plt.figure(2)
+plt.plot(list(range(1, number_of_generations)), plots['gen i-1 vs gen i'])
+plt.xlabel('generation number')
+plt.ylabel('score vs previous generation')
+plt.savefig('plot_3.png')
